6f8374538c86cd7b9ea6c139e3230e039ce2fc6d
Ujjwal Verma <vujjwal1n@gmail.com>
2022-03-09 13:01:09 +0530

    Added Container Component React Bootstrap

--- SmartGrid/smart-grid/src/components/Home.js
+++ SmartGrid/smart-grid/src/components/Home.js
@@ -1,28 +1,57 @@
 import React, { useState } from "react";
 import Analysis from "./Analysis";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Home = () => {
-  const homeClick = () => {
-  };
+  // const homeClick = () => {};
   const [toggled, setToggled] = useState(false);
 
   /* Define a function that toggles the visibility of the image */
   const toggleImage = () => setToggled(!toggled);
 
   return (
-    <div className="Home">
-      <h2>DASHBOARD</h2>
+    <>
+      <Container fluid>
+        <Row className="justify-content-md-center">
+          <Col>
+            <Button onClick={toggleImage} variant="outline-primary">
+              Analysis
+            </Button>
+          </Col>
+          <Col>
+            <Button variant="outline-warning">
+              Appliances
+            </Button>
+          </Col>
+          <Col>
+            <Button variant="outline-danger">
+              Savings
+            </Button>
+          </Col>
+          <Col>
+            <Button  variant="outline-success">
+              Language
+            </Button>
+          </Col>
+        </Row>
+        <Row>
+        {toggled && <Analysis></Analysis>}
+        </Row>
+      </Container>
+    </>
+    // <div className="Home">
+    //   <h2>DASHBOARD</h2>
 
-      <br />
-      <div>
-        {<Button onClick={toggleImage} variant="outline-primary">Analysis</Button>}
-        {<Button onClick={toggleImage} variant="outline-warning">Appliances</Button>}
-        {<Button onClick={toggleImage} variant="outline-danger">Savings</Button>}
-        {<Button onClick={toggleImage} variant="outline-success">Language</Button>}
-      </div>
-      <div>{toggled && <Analysis></Analysis>}</div>
-    </div>
+    //   <br />
+    //   <div>
+    //     {<Button onClick={toggleImage} variant="outline-primary">Analysis</Button>}
+    //     {<Button onClick={toggleImage} variant="outline-warning">Appliances</Button>}
+    //     {<Button onClick={toggleImage} variant="outline-danger">Savings</Button>}
+    //     {<Button onClick={toggleImage} variant="outline-success">Language</Button>}
+    //   </div>
+    //   <div>{toggled && <Analysis></Analysis>}</div>
+    // </div>
   );
 };
 
