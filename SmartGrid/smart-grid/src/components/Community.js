56012457f2ca168898dcf3740c69a5667be61a13
apurbashakya <apurbashakya333@gmail.com>
2022-03-09 21:48:57 +0545

    Card Addition

--- SmartGrid/smart-grid/src/components/Community.js
+++ SmartGrid/smart-grid/src/components/Community.js
@@ -1,12 +1,30 @@
+import React from "react";
+import { Card } from "react-bootstrap";
 const Community = () => {
-  return (
-    <div>
-      <h1>Join Community</h1>
-      <a href="https://www.reddit.com/r/Elcon_Energy/">Reddit</a>
-      <br></br>
-      <a href="https://discord.gg/ZuF9Q9Kb">Discord</a>
-    </div>
-  );
-};
-
+    const cardInfo = [
+        {
+            image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.0kj7XPF_bdTVCg7IFuPR-gHaFV%26pid%3DApi&f=1",
+            title: "Discord",
+            link: <a href="https://discord.gg/ZuF9Q9Kb">JOIN US ON DISCORD</a>,
+        },
+        {
+            image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.8CqhYk-Xsj377uERoYrMCgHaEK%26pid%3DApi&f=1",
+            title: "Reddit",
+            link: <a href="https://www.reddit.com/r/Elcon_Energy/">JOIN US ON REDDIT</a>,
+        },
+    ];
+    const renderCard = (card, index) => {
+        return (
+            <div><Card style={{ width: "13rem" }} key={index} className="box">
+                <Card.Body>
+                    <Card.Title>{card.title}</Card.Title>
+                    <Card.Img variant="top" src="holder.js/100px180" src={card.image} />
+                    <Card.Text>{card.link}</Card.Text>
+                </Card.Body>
+            </Card>
+            </div>
+        );
+    };
+    return <div className="grid">{cardInfo.map(renderCard)}</div>;
+}
 export default Community;
